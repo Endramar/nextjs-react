5022232a8f46e24ac89c4d24fd8962b586f2b3fe
Endramar <alikskyl@gmail.com>
2020-07-05 15:12:28 +0300

    handling 404 errors : custom error page

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 
+import User from '../../components/User';
+
 const authPage = () => {
     return <div>
         <h1>The auth index page.</h1>
+        <User name="Ali" age="31"></User>
         <p>Go back to <Link href="/"><a>Main Page</a></Link></p>
     </div>;
 }
